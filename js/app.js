75a3cf2b54df6c4e9eed8bd01eb24c58ecc5a411
Carlos Felipe Diaz Lombana <server-77@outlook.com>
2018-10-17 13:03:12 -0500

    entrar

--- js/app.js
+++ js/app.js
@@ -1,40 +1,39 @@
-$(function(){
+$(function() {
+  
+
   //efectos wow.js
   new WOW().init();
 
   //flecha abajo
-  $(".flecha-abajo a").on('click', function(event) {
+  $(".flecha-abajo a").on("click", function(event) {
     event.preventDefault();
-    var hash = this.hash;//obtiene la ruta #..
-    $('html, body').animate({
+    var hash = this.hash; //obtiene la ruta #..
+    $("html, body").animate(
+      {
         scrollTop: $(hash).offset().top
-    }, 900, function(){
+      },
+      900,
+      function() {
         window.location.hash = hash;
-    });
+      }
+    );
   });
 
   //botón servicios -->#link-servicios
-  $(".navbar-nav li a").on('click', function(event) {
+  $(".navbar-nav li a").on("click", function(event) {
     // event.preventDefault();-------------------------------Esto me generaba el error de que no me cambia de pagina xD
-    var hash = this.hash;//obtiene la ruta #..
-    $('html, body').animate({
+    var hash = this.hash; //obtiene la ruta #..
+    $("html, body").animate(
+      {
         scrollTop: $(hash).offset().top
-    }, 900, function(){
+      },
+      900,
+      function() {
         window.location.hash = hash;
-    });
+      }
+    );
   });
 
   var scroll = ScrollReveal();
   // scroll.reveal('.row',{duration: 3000});
-
-  $(".loader").fadeOut(function(){
-    $("#content").removeClass("invisible").addClass("visible");
-});
-
-
-
-
-
-
-
 });
