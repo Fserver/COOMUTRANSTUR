65f41d4d3e74d8b4a738cf8add26b9f6ff12928a
Carlos Felipe Diaz Lombana <server-77@outlook.com>
2018-10-30 13:30:44 -0500

    index

--- js/app.js
+++ js/app.js
@@ -57,3 +57,30 @@ $(function() {
   // scroll.reveal('.row',{duration: 3000});
 
 });
+
+
+
+// Videotutorial: https://codepen.io/escueladigital/pen/OyLYrW
+$(window).on('scroll',parallax);
+
+function parallax(){
+  //obtener el nivel de scroll
+  var s = $(window).scrollTop();
+  // efecto parallax para los fondos
+  function parallaxBg(el,t){
+    $(el).css({
+      'background-attachment': 'fixed',
+      'background-position': 'center ' + -(s*t) + 'px' 
+    })
+  }
+  // efecto parallax para los textos
+  function parallaxFront(el,t){
+    $(el).css({
+      'position': 'relative',
+      'top': -(s*t) + 'px'
+    })
+  } 
+
+  parallaxBg('#parallax1',.2);
+  // parallaxFront('.page',.4);
+}
