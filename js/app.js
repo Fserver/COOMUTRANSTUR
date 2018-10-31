eea40a4b8feda29637bc9bf84466e89c1016190d
Carlos Felipe Diaz Lombana <server-77@outlook.com>
2018-10-30 22:30:14 -0500

    .

--- js/app.js
+++ js/app.js
@@ -1,16 +1,5 @@
 $(function () {
 
-  (function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v3.1&appId=642215239509270&autoLogAppEvents=1';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-
-
-
-
   jQuery(document).ready(function ($) {
     // site preloader -- also uncomment the div in the header and the css style for #preloader
     $(window).load(function () {
