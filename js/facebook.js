0f083bf6afae0994d0e650476abb0d2d9aa6c380
Carlos Felipe Diaz Lombana <server-77@outlook.com>
2018-11-23 12:20:22 -0500

    modificaciones index
    detalles de imagenes y redes sociales

--- js/facebook.js
+++ js/facebook.js
@@ -5,3 +5,11 @@
   js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v3.2&appId=642215239509270&autoLogAppEvents=1';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v3.2&appId=642215239509270&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
