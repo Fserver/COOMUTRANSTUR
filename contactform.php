b797cff6714c669bb3354470ba6e594cb9f0dd5a
Carlos Felipe Diaz Lombana <server-77@outlook.com>
2018-11-23 22:54:32 -0500

    Listo para sincronizar
    10:54pm

--- contactform.php
+++ contactform.php
@@ -4,18 +4,16 @@
     $correo = $_POST['email'];
     $telefono = $_POST['phone'];
     $mensaje = $_POST['message'];
-
     // Datos para el correo
     $destinatario = "iluminacion7@gmail.com";
     $asunto = "Contacto desde nuestra web";
-
     $carta = "De: $nombre \n";
     $carta .= "Correo: $correo \n";
     $carta .= "Telefono: $telefono \n";
     $carta .= "Mensaje: $mensaje";
-
+    $headers = "From: admin@coomutranstur.com\r\n";
     // Enviando Mensaje
-    mail($destinatario, $asunto, $carta);
+    mail($destinatario, $asunto, $carta, $headers);
     header('Location:/index.html');
-
+    echo "Mail Sent.";
 ?>
