1869481659471ecf17e7d60aede106e8138836d2
Carlos Felipe Diaz Lombana <server-77@outlook.com>
2018-11-24 16:32:17 -0500

    actualizacion
    4:32pm

--- js/app.js
+++ js/app.js
@@ -41,129 +41,144 @@ $(function () {
     );
   });
 
+  inView('.galery_container').once('enter', function () {
+    $('.lazy').lazy({
+      placeholder: "data:img/load.gif;",
+      threshold: 0,
+      visibleOnly: true
+    });
+  });
+  $('a.gallery').featherlightGallery({
+    previousIcon: '«',
+    nextIcon: '»',
+    galleryFadeIn: 300,
 
- /* ADMINISTRADOR */
+    openSpeed: 300
+  });
 
 
- /**WYSiWYG */
-
- /**Wysiwyg */
+  /* ADMINISTRADOR */
 
 
- /**Registrar noticia */
+  /**WYSiWYG */
 
- $(document).on("submit", "#form-noticia", function (e) {
-   e.preventDefault();
-   $.ajax({
-     type: "POST",
-     url: "../services/registrarNoticia.php",
-     data: new FormData(this),
-     contentType: false,
-     cache: false,
-     processData:false,
-     dataType: "JSON",
-     beforeSend: function(){
-       $('#sbmButton').attr("disabled","disabled");
-       $('#form-noticia').css("opacity",".5");
-     },
-     success: function (response) {
-       $('#form-noticia').css("opacity","");
-       if(response.rpta == true){
-         alert("Registro exitoso");
-         location.href ="https://"+host+"/admin/noticias.php";
-       }else
-       if(response.rpta == false){
-         alert("No se pudo registrar");
-       }
-       if(response.rpta === "err"){
-         location.href ="https://"+host+"/autenticacion.php";
-       }
-     }
-   });
- });
+  /**Wysiwyg */
 
- /**Editar noticia */
- $(document).on("submit", "#form-editar-noticia", function (e) {
-   e.preventDefault();
-   var data = new FormData(this);
-   var fileImage = $("#fileImage").val();
-   var bandera;
-   if(fileImage != ""){
-     bandera = "true";
-   }else{
-     bandera = "false";
-   }
-   data.append("bandera", bandera);
 
-   $.ajax({
-     type: "POST",
-     url: "../services/editarNoticia.php",
-     data: data,
-     contentType: false,
-     cache: false,
-     processData:false,
-     dataType: "JSON",
-     beforeSend: function(){
-       $('#sbmButton').attr("disabled","disabled");
-       $('#form-editar-noticia').css("opacity",".5");
-     },
-     success: function (response) {
-       $('#form-editar-noticia').css("opacity","");
-       $('#sbmButton').attr("disabled",false);
-       if(response.rpta == true){
-         alert("Edición exitosa");
-         location.href ="https://"+host+"/admin/noticias.php";
-       }else
-       if(response.rpta == false){
-         alert("No se pudo editar");
-       }
-       if(response.rpta === "err"){
-         location.href ="https://"+host+"/autenticacion.php";
-       }
-     }
-   });
- });
+  /**Registrar noticia */
 
- //file type validation
- $("#fileImage").change(function() {
-   var file = this.files[0];
-   var imagefile = file.type;
-   var match= ["image/jpeg","image/png","image/jpg"];
-   if(!((imagefile==match[0]) || (imagefile==match[1]) || (imagefile==match[2]))){
-     alert('Por favor selecciona un tipo de imagen válido (JPEG/JPG/PNG).');
-       $("#fileImage").val('');
-       return false;
-     }
- });
- 
- /**Eliminar noticia */
- $(document).on("click", "#status-noticia", function (e) {
-   e.preventDefault();
-   var id = $(this).data("noticia");
-   var estado = $(this).data("status-noticia");
-   data = {
-     "id" : id,
-     "estado" : estado
-   }
-   $.ajax({
-     type: "POST",
-     url: "../services/status-noticia.php",
-     data: data,
-     dataType: "JSON",
-     success: function (response) {
-       if(response.rpta == true){
-         location.href ="https://"+host+"/admin/noticias.php";
-       }else
-       if(response.rpta == false){
-         alert("Ocurrió un error. Comunícate con el administrador del sistema.");
-       }
-     }
-   });
- });
-   /* ADMINISTRADOR */
+  $(document).on("submit", "#form-noticia", function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "../services/registrarNoticia.php",
+      data: new FormData(this),
+      contentType: false,
+      cache: false,
+      processData: false,
+      dataType: "JSON",
+      beforeSend: function () {
+        $('#sbmButton').attr("disabled", "disabled");
+        $('#form-noticia').css("opacity", ".5");
+      },
+      success: function (response) {
+        $('#form-noticia').css("opacity", "");
+        if (response.rpta == true) {
+          alert("Registro exitoso");
+          location.href = "https://" + host + "/admin/noticias.php";
+        } else
+          if (response.rpta == false) {
+            alert("No se pudo registrar");
+          }
+        if (response.rpta === "err") {
+          location.href = "https://" + host + "/autenticacion.php";
+        }
+      }
+    });
+  });
+
+  /**Editar noticia */
+  $(document).on("submit", "#form-editar-noticia", function (e) {
+    e.preventDefault();
+    var data = new FormData(this);
+    var fileImage = $("#fileImage").val();
+    var bandera;
+    if (fileImage != "") {
+      bandera = "true";
+    } else {
+      bandera = "false";
+    }
+    data.append("bandera", bandera);
+
+    $.ajax({
+      type: "POST",
+      url: "../services/editarNoticia.php",
+      data: data,
+      contentType: false,
+      cache: false,
+      processData: false,
+      dataType: "JSON",
+      beforeSend: function () {
+        $('#sbmButton').attr("disabled", "disabled");
+        $('#form-editar-noticia').css("opacity", ".5");
+      },
+      success: function (response) {
+        $('#form-editar-noticia').css("opacity", "");
+        $('#sbmButton').attr("disabled", false);
+        if (response.rpta == true) {
+          alert("Edición exitosa");
+          location.href = "https://" + host + "/admin/noticias.php";
+        } else
+          if (response.rpta == false) {
+            alert("No se pudo editar");
+          }
+        if (response.rpta === "err") {
+          location.href = "https://" + host + "/autenticacion.php";
+        }
+      }
+    });
+  });
+
+  //file type validation
+  $("#fileImage").change(function () {
+    var file = this.files[0];
+    var imagefile = file.type;
+    var match = ["image/jpeg", "image/png", "image/jpg"];
+    if (!((imagefile == match[0]) || (imagefile == match[1]) || (imagefile == match[2]))) {
+      alert('Por favor selecciona un tipo de imagen válido (JPEG/JPG/PNG).');
+      $("#fileImage").val('');
+      return false;
+    }
+  });
+
+  /**Eliminar noticia */
+  $(document).on("click", "#status-noticia", function (e) {
+    e.preventDefault();
+    var id = $(this).data("noticia");
+    var estado = $(this).data("status-noticia");
+    data = {
+      "id": id,
+      "estado": estado
+    }
+    $.ajax({
+      type: "POST",
+      url: "../services/status-noticia.php",
+      data: data,
+      dataType: "JSON",
+      success: function (response) {
+        if (response.rpta == true) {
+          location.href = "https://" + host + "/admin/noticias.php";
+        } else
+          if (response.rpta == false) {
+            alert("Ocurrió un error. Comunícate con el administrador del sistema.");
+          }
+      }
+    });
+  });
+  /* ADMINISTRADOR */
 
   /**MANAGER */
- /**Registrar administrador */
+  /**Registrar administrador */
 
   $(document).on("submit", "#form-administrador", function (e) {
     e.preventDefault();
@@ -173,22 +188,22 @@ $(function () {
       url: "../services/registrarAdministrador.php",
       data: data,
       dataType: "JSON",
-      beforeSend: function(){
-        $('#sbmButton').attr("disabled","disabled");
-        $('#form-administrador').css("opacity",".5");
+      beforeSend: function () {
+        $('#sbmButton').attr("disabled", "disabled");
+        $('#form-administrador').css("opacity", ".5");
       },
       success: function (response) {
-        $('#form-administrador').css("opacity","");
-        if(response.rpta == true){
+        $('#form-administrador').css("opacity", "");
+        if (response.rpta == true) {
           alert("Registro exitoso");
-          location.href ="https://"+host+"/manager/gestion-administradores.php";
-        }else
-        if(response.rpta == false){
-          alert("No se pudo registrar");
-          $('#sbmButton').attr("disabled",false);
-        }
-        if(response.rpta === "err"){
-          location.href ="https://"+host+"/autenticacion.php";
+          location.href = "https://" + host + "/manager/gestion-administradores.php";
+        } else
+          if (response.rpta == false) {
+            alert("No se pudo registrar");
+            $('#sbmButton').attr("disabled", false);
+          }
+        if (response.rpta === "err") {
+          location.href = "https://" + host + "/autenticacion.php";
         }
       }
     });
@@ -202,7 +217,7 @@ $(function () {
       url: "../services/editarAdministrador.php",
       data: data,
       dataType: "JSON",
-      beforeSend: function(){
+      beforeSend: function () {
         // $('#sbmButton').attr("disabled","disabled");
         // $('#form-administrador').css("opacity",".5");
       },
@@ -210,15 +225,15 @@ $(function () {
         console.log(response);
         // $('#form-administrador').css("opacity","");
         // $('#sbmButton').attr("disabled",false);
-        if(response.rpta == true){
+        if (response.rpta == true) {
           alert("Edición exitosa");
-          location.href ="https://"+host+"/manager/gestion-administradores.php";
-        }else
-        if(response.rpta == false){
-          alert("No se pudo editar");
-        }
-        if(response.rpta === "err"){
-          location.href ="https://"+host+"/autenticacion.php";
+          location.href = "https://" + host + "/manager/gestion-administradores.php";
+        } else
+          if (response.rpta == false) {
+            alert("No se pudo editar");
+          }
+        if (response.rpta === "err") {
+          location.href = "https://" + host + "/autenticacion.php";
         }
       }
     });
@@ -230,12 +245,12 @@ $(function () {
 
   /**NEWS */
 
-  $(document).on("keyup", "#input-search", function(){
+  $(document).on("keyup", "#input-search", function () {
     var suggestions = $("#search-suggestions");
     suggestions.html("");
     var value = $(this).val();
     var data = {
-      "search" : value
+      "search": value
     }
     $.ajax({
       type: "POST",
@@ -243,10 +258,10 @@ $(function () {
       data: data,
       dataType: "JSON",
       success: function (response) {
-        if(response.rpta == true){
-          $.each(response, function (i, valor) { 
-            if(valor.titulo != undefined){
-              suggestions.append('<option label="'+valor.titulo+'" value="'+valor.titulo+'" data-id="'+valor.idnoticia+'">');
+        if (response.rpta == true) {
+          $.each(response, function (i, valor) {
+            if (valor.titulo != undefined) {
+              suggestions.append('<option label="' + valor.titulo + '" value="' + valor.titulo + '" data-id="' + valor.idnoticia + '">');
             }
           });
         }
@@ -258,20 +273,20 @@ $(function () {
   $(document).on("input", "#input-search", function (e) {
     // e.preventDefault();
     var userText = $(this).val();
-      $("#search-suggestions").find("option").each(function() {
-        if ($(this).val() === userText) {
-          if($(this).val() != ""){
-            var id = $(this).data("id");
-            location.href ="https://"+host+"/news/noticia.php?id="+id;
-          }
+    $("#search-suggestions").find("option").each(function () {
+      if ($(this).val() === userText) {
+        if ($(this).val() != "") {
+          var id = $(this).data("id");
+          location.href = "https://" + host + "/news/noticia.php?id=" + id;
         }
-      });
-    
+      }
+    });
+
   });
 
 
-   /**NEWS */
-   
+  /**NEWS */
+
 });
 
 function googleTranslateElementInit() {
