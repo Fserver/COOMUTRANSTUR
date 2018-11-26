bfeebbf9d8c2b24b429287754bd8ed2e97d918d6
Carlos Felipe Diaz Lombana <server-77@outlook.com>
2018-11-25 21:53:24 -0500

    cosas de noticias
    9:53pm

--- js/app.js
+++ js/app.js
@@ -41,22 +41,6 @@ $(function () {
     );
   });
 
-  inView('.galery_container').once('enter', function () {
-    $('.lazy').lazy({
-      placeholder: "data:img/load.gif;",
-      threshold: 0,
-      visibleOnly: true
-    });
-  });
-  $('a.gallery').featherlightGallery({
-    previousIcon: '«',
-    nextIcon: '»',
-    galleryFadeIn: 300,
-
-    openSpeed: 300
-  });
-
-
   /* ADMINISTRADOR */
 
 
--- js/carga.js
+++ js/carga.js
@@ -1,7 +1,18 @@
 $(function () {
-    $('.lazy').lazy({
-        placeholder: "data:img/load.gif;",
-        threshold: 0,
-        visibleOnly: true
+    inView('.galery_container').once('enter', function () {
+        $('.lazy').lazy({
+            placeholder: "data:img/load.gif;",
+            threshold: 0,
+            visibleOnly: true
+        });
     });
-  });
+    $('a.gallery').featherlightGallery({
+        previousIcon: '«',
+        nextIcon: '»',
+        galleryFadeIn: 300,
+
+        openSpeed: 300
+    });
+
+});
+
